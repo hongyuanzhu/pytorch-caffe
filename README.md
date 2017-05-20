fd45162229b65952fb2e2a4042474519cd52e378
xh <hang_xiao_cn@163.com>
2017-05-20 12:34:41 +0800

    parse prototxt ok

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pytorch-caffe
+train caffe models on pytorch
+
+
+# Todo
+- [x] parse prototxt
+- [x] parse solver
+- [ ] create network
+- train mnist
---
+++ cfg.py
@@ -0,0 +1,69 @@
+def parse_prototxt(protofile):
+    def line_type(line):
+        if line.find(':') >= 0:
+            return 0
+        elif line.find('{') >= 0:
+            return 1
+        return -1
+
+    def parse_block(fp):
+        block = dict()
+        line = fp.readline().strip()
+        while line != '}':
+            ltype = line_type(line)
+            if ltype == 0: # key: value
+                key, value = line.split(':')
+                key = key.strip()
+                value = value.strip().strip('"')
+                if block.has_key(key):
+                    if type(block[key]) == list:
+                        block[key].append(value)
+                    else:
+                        block[key] = [block[key], value]
+                else:
+                    block[key] = value
+            elif ltype == 1: # blockname {
+                key = line.split('{')[0].strip()
+                sub_block = parse_block(fp)
+                block[key] = sub_block
+            line = fp.readline().strip()
+            line = line.split('#')[0]
+        return block
+
+    fp = open(protofile, 'r')
+    props = dict()
+    layers = []
+    line = fp.readline()
+    while line != '':
+        line = line.strip().split('#')[0]
+        if line == '':
+            line = fp.readline()
+            continue
+        ltype = line_type(line)
+        if ltype == 0: # key: value
+            key, value = line.split(':')
+            key = key.strip()
+            value = value.strip().strip('"')
+            props[key] = value
+        elif ltype == 1: # blockname {
+            key = line.split('{')[0].strip()
+            if key == 'layer':
+                layer = parse_block(fp)
+                layers.append(layer)
+            else:
+                props[key] = parse_block(fp)
+        line = fp.readline()
+
+    if len(layers) > 0:
+        net_info = dict()
+        net_info['props'] = props
+        net_info['layers'] = layers
+    else:
+        return props
+
+if __name__ == '__main__':
+    import sys
+    protofile = sys.argv[1]
+    print(protofile)
+    net_info = parse_prototxt(protofile)
+    print(net_info)
